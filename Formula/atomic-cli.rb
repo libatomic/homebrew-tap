700c0e9f3f476b82380220d3003fb47c47444734
Rob Rodriguez <rob@modelrocket.io>
2025-08-09 19:54:31 -0700

    updating tap

---
+++ Formula/atomic-cli.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class AtomicCli < Formula
+  desc "A command-line tool for Atomic"
+  homepage "https://github.com/libatomic/atomic-cli"
+  url "https://github.com/libatomic/atomic-cli/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "2cea8136da96d05143b2603b1fe7afcedb0fde78ea0ab801d30897390cc24816"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test atomic-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
